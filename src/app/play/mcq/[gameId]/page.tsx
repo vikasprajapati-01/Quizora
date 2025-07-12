a8421b78bb283fb27f28d93754bfc8caff3c76b0
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2025-07-12 22:31:35 +0530

    production changes in mcq play

--- src/app/play/mcq/[gameId]/page.tsx
+++ src/app/play/mcq/[gameId]/page.tsx
@@ -1,46 +1,41 @@
 import React from "react";
-
 import { getAuthSession } from "@/lib/next-auth";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/database";
-
 import MCQ from "@/components/MCQ";
 
 type Props = {
-  params: {
-    gameId: string;
-  }
+  params: Promise<{ gameId: string }>;
 };
 
-const MCQPage = async ({params: {gameId}} : Props) => {
+const MCQPage = async ({ params }: Props) => {
+  const { gameId } = await params; // Await params
 
-    const session = await getAuthSession();
-    if(!session?.user) {
-        return redirect("/");
-    }
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
 
-    const game = await prisma.game.findUnique({
-        where: {
-            id: gameId,
+  const game = await prisma.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: {
+        select: {
+          id: true,
+          question: true,
+          options: true,
         },
-        include: {
-            questions: {
-                select: {
-                id: true,
-                question: true,
-                options: true,
-                },
-            },
-        },
-    });
+      },
+    },
+  });
 
-    if (!game || game.gameType !== 'mcq') {
-        return redirect("/quiz");
-    }
+  if (!game || game.gameType !== "mcq") {
+    return redirect("/quiz");
+  }
 
-  return (
-    <MCQ game={game} />
-  );
-}
+  return <MCQ game={game} />;
+};
 
 export default MCQPage;
